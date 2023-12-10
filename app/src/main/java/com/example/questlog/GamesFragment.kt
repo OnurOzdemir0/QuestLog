13c995d0e5f6df6e67cba8cd737d1d0ec4b6ef1d
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2023-12-11 00:15:19 +0300

    main fragment functionality, games fragment mocks

--- app/src/main/java/com/example/questlog/GamesFragment.kt
+++ app/src/main/java/com/example/questlog/GamesFragment.kt
@@ -1,59 +1,55 @@
 package com.example.questlog
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.SearchView
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GamesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GamesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var searchView: SearchView
+    private lateinit var gamesRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_games, container, false)
+        val view = inflater.inflate(R.layout.fragment_games, container, false)
+        searchView = view.findViewById(R.id.search_view)
+        gamesRecyclerView = view.findViewById(R.id.games_recycler_view)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GamesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GamesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        gamesRecyclerView.layoutManager = LinearLayoutManager(context)
+        gamesRecyclerView.adapter = GamesAdapter(getMockGames())
     }
+
+    private fun getMockGames(): List<String> {
+        return listOf("Game 1", "Game 2", "Game 3", "Game 4")
+    }
+}
+
+class GamesAdapter(private val gamesList: List<String>) : RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(android.R.id.text1)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = gamesList[position]
+    }
+
+    override fun getItemCount(): Int = gamesList.size
 }
--- app/src/main/java/com/example/questlog/MainActivity.kt
+++ app/src/main/java/com/example/questlog/MainActivity.kt
@@ -1,59 +1,28 @@
 package com.example.questlog
 
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.questlog.ui.theme.QuestLogTheme
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val navController by lazy {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navHostFragment.navController
-    }
-
-
-    private val appBarConfiguration: AppBarConfiguration by lazy {
-        AppBarConfiguration(setOf(R.id.mainFragment, R.id.gamesFragment, R.id.listsFragment))
-    }
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupNavigation()
-    }
 
-    private fun setupNavigation() {
-        // Setup ActionBar with NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.mainFragment, R.id.gamesFragment, R.id.listsFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        findViewById<Button>(R.id.button_games).setOnClickListener {
-            navController.navigate(R.id.action_mainFragment_to_gamesFragment)
-        }
-        findViewById<Button>(R.id.button_lists).setOnClickListener {
-            navController.navigate(R.id.action_mainFragment_to_listsFragment)
-        }
-        findViewById<Button>(R.id.button_reviews).setOnClickListener {
-            navController.navigate(R.id.action_mainFragment_to_reviewsFragment)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
