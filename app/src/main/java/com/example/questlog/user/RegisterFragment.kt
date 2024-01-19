d9b833321e6c0d79532ca06e3ad59c3f513685bb
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2024-01-19 21:18:23 +0300

    Update RegisterFragment.kt

--- app/src/main/java/com/example/questlog/user/RegisterFragment.kt
+++ app/src/main/java/com/example/questlog/user/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.example.questlog.user
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,6 +28,7 @@ class RegisterFragment : Fragment() {
 
         binding.registerButton.setOnClickListener{
             //TODO upload user to data base
+            Log.d("RegisterFragment", "Register button clicked")
             navController.navigate(R.id.action_registerFragment_to_logInFragment)
         }
         return inflater.inflate(R.layout.fragment_register, container, false)
