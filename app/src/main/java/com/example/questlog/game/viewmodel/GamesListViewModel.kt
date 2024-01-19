46988937ec96a9831a6e0c23f1508772cb35c325
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2024-01-19 04:19:09 +0300

    temp fix on games page & import error fix

--- app/src/main/java/com/example/questlog/game/viewmodel/GamesListViewModel.kt
+++ app/src/main/java/com/example/questlog/game/viewmodel/GamesListViewModel.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import androidx.lifecycle.*
 import com.example.questlog.database.GameDatabase
 import com.example.questlog.game.GameItem
-import kotlinx.coroutines.flow.internal.NopCollector.emit
+
 
 
 class GamesListViewModel: ViewModel() {
