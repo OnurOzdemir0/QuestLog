111b85b6e066065c66a531a62bdaef9fcedb01c8
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2024-01-20 17:57:07 +0300

    Update GameDatabase.kt

--- app/src/main/java/com/example/questlog/database/GameDatabase.kt
+++ app/src/main/java/com/example/questlog/database/GameDatabase.kt
@@ -20,7 +20,7 @@ class GameDatabase {
     suspend fun getGames(): List<GameItem> {
         return try {
             val gameDocuments = db.collection("games")
-                .limit(50)
+                .limit(200)
                 .get()
                 .await()
 
